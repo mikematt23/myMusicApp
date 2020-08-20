72dedb902ae766950cb7f804ef6307f418e7ef95
evantwidwell <evanski@gmail.com>
2020-08-20 18:54:28 -0500

    more changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
-
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
 import Login from './Components/Login'
 
 import NavBar from './Components/Navbar'
 import SimpleCard from './Components/Switch'
 
+import TextField from '@material-ui/core/TextField';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+const changeState = ()=>{
+  this.setState({
+    isLoggedIn: !this.state.isLoggedIn
+  })
+  console.log(this.state.isLoggedIn)
+}
 
 class App extends React.Component{
   constructor(props){
@@ -25,6 +41,7 @@ class App extends React.Component{
     console.log(this.state.isLoggedIn)
   }
 
+
   render(){
     if(this.state.isLoggedIn){
       return(
@@ -35,17 +52,17 @@ class App extends React.Component{
       )
     } else{
       return(
-        <div>
+        <div >
 
         <NavBar></NavBar>
-          <form>
-            <input type ='text'/>
-            <br/>
-            <input type ='password'/>
-            <br/>
-            < Button onClick ={this.changeState}> Log In</ Button>
-          </form>
-
+        <div className="SignIn">
+        <form  noValidate autoComplete="off">
+          <TextField id="standard-basic" label="UserName" /><br/>
+          <TextField id="standard-basic" label="Password" /><br/><br/>
+          < Button variant="contained" color="primary" onClick ={this.changeState}> Log In</ Button>
+         
+    </form>
+    </div>
           
           <h1></h1>
 
@@ -56,3 +73,4 @@ class App extends React.Component{
 }
 
 export default App;
+
